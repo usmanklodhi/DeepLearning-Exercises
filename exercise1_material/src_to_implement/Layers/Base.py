af316b6fdc16e6492c1f56dd92f7500e0b5eb1ae
mkhodj96 <134135848+mkhodj96@users.noreply.github.com>
2024-05-12 15:41:35 +0200

    implemented circle, modified .gitignore

---
+++ exercise1_material/src_to_implement/Layers/Base.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+class BaseLayer(object):
+    def __init__(self):
+        self.trainable = False
+        # with random values drawn from a uniform distribution,which can provide a good starting point for
+        # optimization algorithms like gradient descent.
+        self.weights = self.weights = np.random.uniform(-1, 1, size=(10, 10))
+
