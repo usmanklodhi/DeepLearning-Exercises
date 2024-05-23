8ad576b108dace6e08657d955109f5ff91d8e175
Usman KL <usmanklodhii@gmail.com>
2024-05-24 01:46:58 +0200

    fixed implementation of crossentropyloss, tests passing

--- exercise1_material/src_to_implement/Optimization/Loss.py
+++ exercise1_material/src_to_implement/Optimization/Loss.py
@@ -1,19 +1,29 @@
 import numpy as np
 
+
+# Cross-entropy loss: Loss function used in classification problems, when outputs of neural networks are probability
+# distributions
+
+
 class CrossEntropyLoss:
     def __init__(self):
-        self.prediction_tensor = None
-        self.label_tensor = None
+        self.prediction_tensor = None  # store predictions made by network
+        self.label_tensor = None  # store ground truth labels
 
     def forward(self, prediction_tensor, label_tensor):
+        # prediction_tensor: probabilities predicted by the model for each class (typically output of softmax layer)
         self.prediction_tensor = prediction_tensor
+        # labels/ ground truth labels: possibly one-hot encoded
         self.label_tensor = label_tensor
+        # smallest representable positive number, used to avoid taking log of zero
         epsilon = np.finfo(float).eps
-        # limit the values in an array to a specified range
-        prediction_tensor = np.clip(prediction_tensor, epsilon, 1 - epsilon)
-        loss = -np.sum(label_tensor * np.log(prediction_tensor)) / prediction_tensor.shape[0]
+        # compute cross-entropy loss
+        # take negative log of all predicted probabilities adjusted by epsilon and multiplying corresponding albels
+        # the operation is element-wise, and total loss is computed by summing over all elements
+        loss = -np.sum(label_tensor * np.log(prediction_tensor + epsilon))
         return loss
 
     def backward(self, label_tensor):
-        batch_size = self.prediction_tensor.shape[0]
-        return (self.prediction_tensor - label_tensor) / batch_size
+        # used in backpropagation
+        # return gradient of the loss wrt predictions
+        return - label_tensor / self.prediction_tensor
