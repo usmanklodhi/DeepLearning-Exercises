a383e233c8386fa4d95f4aec05a190f7ca58af66
mkhodj96 <134135848+mkhodj96@users.noreply.github.com>
2024-05-24 21:09:22 +0200

    implemented circle, modified .gitignore

--- exercise1_material/src_to_implement/Optimization/Loss.py
+++ exercise1_material/src_to_implement/Optimization/Loss.py
@@ -10,23 +10,20 @@ class CrossEntropyLoss:
         self.label_tensor = None
         self.prediction_tensor = None  # store predictions made by network
 
-    def forward(self, prediction_tensor,label_tensor):
+    def forward(self, prediction_tensor, label_tensor):
         # prediction_tensor: probabilities predicted by the model for each class (typically output of softmax layer)
-        self.prediction_tensor = prediction_tensor
         # labels/ ground truth labels: possibly one-hot encoded
-        self.label_tensor = label_tensor
-        # smallest representable positive number, used to avoid taking log of zero
-        epsilon = np.finfo(float).eps
+        # the smallest representable positive number, used to avoid taking log of zero
         # compute cross-entropy loss
         # take negative log of all predicted probabilities adjusted by epsilon and multiplying corresponding albels
         # the operation is element-wise, and total loss is computed by summing over all elements
-        loss = -np.sum(label_tensor * np.log(prediction_tensor + epsilon)) / prediction_tensor.shape[0]
+        self.prediction_tensor = prediction_tensor
+        loss = -np.sum(label_tensor * np.log(prediction_tensor + np.finfo(float).eps))
         return loss
 
     def backward(self, label_tensor):
         # used in backpropagation
         # return gradient of the loss wrt predictions
-        batch_size = label_tensor.shape[0]
-        return -label_tensor / (self.prediction_tensor + np.finfo(float).eps) / batch_size
-    #    epsilon = np.finfo(float).eps
-    #    return - label_tensor /(self.prediction_tensor + epsilon)
+        # def backward(self, label_tensor):
+        tmp = -(1 / self.prediction_tensor) * label_tensor
+        return tmp
