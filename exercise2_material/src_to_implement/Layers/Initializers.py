022fb886f4480bd2be0d39d9b09237a6744832b4
Usman KL <usmanklodhii@gmail.com>
2024-06-14 16:32:47 +0200

    fixed initializers tests

--- exercise2_material/src_to_implement/Layers/Initializers.py
+++ exercise2_material/src_to_implement/Layers/Initializers.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 class Constant:
     def __init__(self, value=0.1):
         self.value = value
@@ -8,22 +7,19 @@ class Constant:
     def initialize(self, shape, fan_in=None, fan_out=None):
         return np.full(shape, self.value)
 
-
 class UniformRandom:
     @staticmethod
-    def initialize(self, shape, fan_in=None, fan_out=None):
+    def initialize(shape, fan_in=None, fan_out=None):
         return np.random.uniform(0, 1, size=shape)
-
 
 class Xavier:
     @staticmethod
-    def initialize(self, shape, fan_in, fan_out):
+    def initialize(shape, fan_in, fan_out):
         stddev = np.sqrt(2 / (fan_in + fan_out))
         return np.random.normal(0, stddev, size=shape)
 
-
 class He:
     @staticmethod
-    def initialize(self, shape, fan_in, fan_out=None):
+    def initialize(shape, fan_in, fan_out=None):
         stddev = np.sqrt(2 / fan_in)
         return np.random.normal(0, stddev, size=shape)
